84319c9f94cf5887f01691983fe9c151ba5ace90
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-12-07 23:55:56 +0500

    Implemented services

--- app/src/main/java/com/shevy/test/MainActivity.kt
+++ app/src/main/java/com/shevy/test/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.shevy.test
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 import com.shevy.test.databinding.ActivityMainBinding
+import com.shevy.test.services.MyService
 import com.shevy.test.ui.main.Fragment1
 import com.shevy.test.ui.main.Fragment2
 import com.shevy.test.ui.main.Fragment3
@@ -16,25 +18,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
 
-        binding.btn1.setOnClickListener {
-            val f1 = Fragment1()
-            val ft = supportFragmentManager.beginTransaction()
-            ft.replace(R.id.container, f1)
-            ft.commit()
-        }
+        binding.apply {
+            btnStart.setOnClickListener {
+                startService(Intent(this@MainActivity, MyService::class.java))
+            }
 
-        binding.btn2.setOnClickListener {
-            val f2 = Fragment2()
-            val ft = supportFragmentManager.beginTransaction()
-            ft.replace(R.id.container, f2)
-            ft.commit()
-        }
-
-        binding.btn3.setOnClickListener {
-            val f3 = Fragment3()
-            val ft = supportFragmentManager.beginTransaction()
-            ft.replace(R.id.container, f3)
-            ft.commit()
+            btnStop.setOnClickListener {
+                stopService(Intent(this@MainActivity,MyService::class.java))
+            }
         }
     }
 
