19f6cffe20bd87689d99a915581e6182e72efb03
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-01-21 17:35:06 +0500

    Read and Write Internal Storage Example

--- app/src/main/java/com/shevy/test/MainActivity.kt
+++ app/src/main/java/com/shevy/test/MainActivity.kt
@@ -19,29 +19,34 @@ class MainActivity : AppCompatActivity() {
         val fileData = binding.editData
 
         binding.btnSave.setOnClickListener {
-            val file:String = fileName.text.toString()
-            val data:String = fileData.text.toString()
-            val fileOutputStream: FileOutputStream
-            try {
-                fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
-                fileOutputStream.write(data.toByteArray())
-            } catch (e: FileNotFoundException){
-                e.printStackTrace()
-            }catch (e: NumberFormatException){
-                e.printStackTrace()
-            }catch (e: IOException){
-                e.printStackTrace()
-            }catch (e: Exception){
-                e.printStackTrace()
+            val file: String = fileName.text.toString()
+            val data: String = fileData.text.toString()
+            if (file.trim() != "") {
+                val fileOutputStream: FileOutputStream
+                try {
+                    fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
+                    fileOutputStream.write(data.toByteArray())
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                } catch (e: NumberFormatException) {
+                    e.printStackTrace()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+                Toast.makeText(applicationContext, "Data save", Toast.LENGTH_LONG).show()
+                fileName.text.clear()
+                fileData.text.clear()
+            } else {
+                Toast.makeText(applicationContext, "File name cannot be blank", Toast.LENGTH_LONG)
+                    .show()
             }
-            Toast.makeText(applicationContext,"Data save",Toast.LENGTH_LONG).show()
-            fileName.text.clear()
-            fileData.text.clear()
         }
 
         binding.btnView.setOnClickListener {
             val filename = fileName.text.toString()
-            if(filename.trim()!=""){
+            if (filename.trim() != "") {
                 var fileInputStream: FileInputStream? = null
                 fileInputStream = openFileInput(filename)
                 val inputStreamReader = InputStreamReader(fileInputStream)
@@ -56,8 +61,13 @@ class MainActivity : AppCompatActivity() {
                 }
                 //Displaying data on EditText
                 fileData.setText(stringBuilder.toString()).toString()
-            }else{
-                Toast.makeText(applicationContext,"File name cannot be blank",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "File name cannot be blank",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
             }
         }
 
